538b0f778d9be2f36cec2dcca3f8334c1369e828
Levi Culp <levi.culp@my.simpson.edu>
2021-09-02 15:18:41 -0500

    lab 1

--- Testing/test.py
+++ Testing/test.py
@@ -1,1 +1,4 @@
-print(
+print("I am really looking forward to the NFL season to start.")
+print("Even my friends think that my favorite team has a great chance this season.")
+print("They said,\"The Cleveland Browns might mess around and win a super bowl\"")
+print("So it is safe for me to say that this season could be a great one.")
